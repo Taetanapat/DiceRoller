ebf7757c56c78f5d05fb41c8f613f6919ead508c
Tanapat Tansara <tanapat_5365@hotmail.com>
2019-08-07 18:03:11 +0700

    Task: Change the text

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -3,17 +3,28 @@ package com.example.dice
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var rollButton: Button = findViewById(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+
+
     }
     private fun rollDice(){
+        val randomInt = java.util.Random().nextInt(6)+1
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = randomInt.toString()
         Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
     }
 }
